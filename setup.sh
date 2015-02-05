4aa0a39b76af55733941e232352e3ec6f150d58f
Ryan Miller <rmill865@gmail.com>
2015-02-05 22:19:39 +0100

    Moved over files from NesPi

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Add Init Scripts
+sudo cp daemons/nes-keypress /etc/init.d/nes-keypress
+sudo chmod 775 /etc/init.d/nes-keypress
+sudo update-rc.d nes-keypress defaults
+
+# Setup NES controller driver
+sudo cp nes-keypress.py /usr/bin/nes-keypress.py
+sudo mkdir /.nes-keypress
+sudo cp configs/controller1.json /.nes-keypress/controller1.json
+sudo cp configs/controller2.json /.nes-keypress/controller2.json
+
+# Install the uinput library
+cd lib/python-uinput-0.9
+sudo python setup.py install --prefix=/usr/local
+sudo python modprobe uinput
+
+# Reboot the Pi
+echo "******** INSTALLATION COMPLETE ********"
+echo "******* PRESS ANY KEY TO REBOOT *******"
+read ANY_KEY
+sudo reboot
